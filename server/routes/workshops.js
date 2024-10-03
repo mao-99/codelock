baa25b27bbcc8d56c6bf68e255595cb9ba589091
mao <mubarakodufade@gmail.com>
2024-10-03 11:39:40 -0400

    Updated README

--- server/routes/workshops.js
+++ server/routes/workshops.js
@@ -1,19 +1,23 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import workshopData from '../data/workshops.js'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import workshopsController from '../controllers/workshops.js';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); // Fix the variable name
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send(workshopData)
-})
+router.get('/', workshopsController.getWorkshops);
 
 router.get('/:workshopID', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, '../public/workshop.html'))
-})
+  res.status(200).sendFile(path.resolve(__dirname, '../public/workshop.html'));
+});
 
-export default router
+router.get('/:workshopID', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/workshop.html'));
+});
+
+router.get('/:workshopID', workshopsController.getWorkshop);
+
+export default router;
